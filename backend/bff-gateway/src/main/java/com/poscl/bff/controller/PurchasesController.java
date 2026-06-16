a378d5b9ae24ae4fab4635cd631eb61bba2c1a19
DevIsaac <imundo90@gmail.com>
2026-06-16 00:12:23 -0400

    fix(bff): add missing Slf4j import to PurchasesController

--- backend/bff-gateway/src/main/java/com/poscl/bff/controller/PurchasesController.java
+++ backend/bff-gateway/src/main/java/com/poscl/bff/controller/PurchasesController.java
@@ -1,5 +1,7 @@
 package com.poscl.bff.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
